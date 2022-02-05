f0ceaaef8f616ec895e2ae54ecc502e4fb5e68e6
Matheus eli <matheuselisouzaferreira@hotmail.com>
2022-02-05 17:09:13 -0300

    7-1 SASS Pattern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'space-tourism-website';
-}
+export class AppComponent {}
