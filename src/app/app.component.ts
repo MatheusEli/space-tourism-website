a078cd0a8530661682ff0d4bf36e407228c766fc
Matheus eli <matheuselisouzaferreira@hotmail.com>
2022-02-23 08:21:42 -0300

    menu desktop - mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,9 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
-  
+
   public menu: HTMLElement;
   public closeButton: HTMLElement;
   public mediaQuery = window.matchMedia('(max-width: 768px)');
@@ -14,21 +14,33 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
     this.menu = document.getElementById('menu');
     this.closeButton = document.getElementById('close-button');
+
+    this.verifyMediaMobile(this.mediaQuery);
+    this.mediaQuery.addListener(this.verifyMediaMobile);
   }
-  public openMenu():void{
-    if(this.mediaQuery.matches){
+  public openMenu(): void {
+    if (this.mediaQuery.matches) {
       this.menu.style.opacity = '1';
       this.menu.style.visibility = 'visible';
       this.closeButton.style.display = "block"
     }
   }
-  
-  public closeMenu():void{
-    if(this.mediaQuery.matches){
+
+  public closeMenu(): void {
+    if (this.mediaQuery.matches) {
       this.menu.style.opacity = '0';
       this.menu.style.visibility = 'hidden';
       this.closeButton.style.display = "none";
     }
+  }
+
+  public verifyMediaMobile(x) {
+
+    if (!x.matches) {
+      document.getElementById('menu').style.opacity = '1';
+      document.getElementById('menu').style.visibility = 'visible';
+      document.getElementById('close-button').style.display = "none";
+    } 
   }
 
 }
