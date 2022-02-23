c949ca821609c44cd42b865bb01b097312ef482c
Matheus eli <matheuselisouzaferreira@hotmail.com>
2022-02-23 08:40:43 -0300

    router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,9 +26,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{
-    scrollPositionRestoration: 'disabled'
-  })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { HomeModule } from './home/home.module';
     BrowserModule,
     AppRoutingModule,
     HomeModule,
-    DestinationModule
+    DestinationModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
